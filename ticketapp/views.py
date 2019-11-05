5a7ba091f7d2e22c37afc1aa8c548cb87b8b7079
Mohamad Bastin <mohamadbastin@gmail.com>
2019-11-05 18:55:21 +0330

    fffffdsdaf

--- ticketapp/views.py
+++ ticketapp/views.py
@@ -111,7 +111,7 @@ def set_invoice(profile):
         # token
         url = "http%3A%2F%2Fmoarefe98.ir%2Fticket%2Fpayment%2Fgateway%2Fcallback%2F%3Ftoken%3D" + token
         make_response = requests.post(make, data={"api_key": api_key, "amount": amount,
-                                                  "return_url": ""})
+                                                  "return_url": url})
         status = (make_response.json()["status"])
         # print(1)
         print(status)
